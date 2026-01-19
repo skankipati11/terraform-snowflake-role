be3ff3fa97a87a3096d16c0b128a62348114ee74
skankipati11 <surya.kankipati11@gmail.com>
2026-01-19 13:24:28 +0530

    Changes

--- versions.tf
+++ versions.tf
@@ -9,11 +9,3 @@ terraform {
   }
 }
 
-provider "snowflake" {
-  organization_name = "xodjopc"
-  account_name      = "jtc04659"
-  user              = "SKANKIPATI"
-  role              = "ACCOUNTADMIN"
-  authenticator     = "SNOWFLAKE_JWT"
-  private_key       = var.snowflake_private_key
-}
